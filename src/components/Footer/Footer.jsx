8e338fc5607b7db2de07d81549c57a245d85ec05
Md.Fazlul Haque <94878259+Tuhinbadhon@users.noreply.github.com>
2024-04-17 16:56:16 +0600

    footer responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,22 +3,15 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
+      {/* className="" */}
       <footer
-        className=" place-items-center w-full gap-4 
-            p-10 bg-base-200 text-base-content flex flex-col  "
+        className=" bg-gradient-to-b from-green-100 via-amber-100 to-transparent
+      bg-opacity-5  place-items-center w-full gap-4 
+            bg-base-200 text-base-content flex flex-col py-5 lg:pt-20 lg:mt-20 mt-5 lg:px-32 px-5 "
       >
-        <div className="font-semibold text-2xl text-center mb-4">
-          <p>
-            Copyright © 2024 - All right reserved by{" "}
-            <span className=" text-4xl font-bold max-[450px]:text-2xl bg-gradient-to-r from-green-500 to-[#59C6D2] text-transparent bg-clip-text">
-              FH Estates
-            </span>{" "}
-            Ltd
-          </p>
-        </div>
         <div className="w-full flex  lg:flex-row md:flex-row flex-col gap-4 lg:justify-between ">
           <div className="flex flex-col  text-center  ">
-            <h6 className="footer-title">Services</h6>
+            <h6 className="footer-title ">Services</h6>
             <a className="link link-hover">Plots</a>
             <a className="link link-hover">Interior Design</a>
             <a className="link link-hover">Sell/Buy</a>
@@ -32,6 +25,17 @@ const Footer = () => {
           <div className="text-center">
             <h6 className="footer-title">Social</h6>
             <div className="grid grid-flow-col gap-4 justify-center">
+              <a href="">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  className="fill-current"
+                >
+                  <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+                </svg>
+              </a>
               <a href="">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -54,17 +58,6 @@ const Footer = () => {
                   <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
                 </svg>
               </a>
-              <a href="">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  className="fill-current"
-                >
-                  <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-                </svg>
-              </a>
             </div>
           </div>
 
@@ -85,6 +78,15 @@ const Footer = () => {
               </div>
             </fieldset>
           </form>
+        </div>
+        <div className="font-semibold lg:text-base text-center mb-2">
+          <p>
+            Copyright © 2024 - All right reserved by{" "}
+            <span className=" lg:text-lg font-bold max-[450px]:text-2xl bg-gradient-to-r from-green-500 to-[#59C6D2] text-transparent bg-clip-text">
+              FH Estates
+            </span>{" "}
+            Ltd
+          </p>
         </div>
       </footer>
     </div>
